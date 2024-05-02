699218a0b56a046a09e0f1f9ca05e3bfaebe1d1e
Dheeman Das <84588268+Reliable-Royalty-29@users.noreply.github.com>
2024-05-02 11:37:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,14 @@ import langchain_helper
 
 st.title("Restaurant Name Generator")
 
-cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Japanese", "Italian", "Mexican", "Arabic", "Spanish"))
+cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican", "Arabic", "American"))
 
 if cuisine:
-    response = langchain_helper.test(cuisine)
+    response = langchain_helper.generate_restaurant_name_and_items(cuisine)
     st.header(response['restaurant_name'].strip())
     menu_items = response['menu_items'].strip().split(",")
     st.write("**Menu Items**")
     for idx, item in enumerate(menu_items, start=1):
-        st.write(f"{idx}. {item.strip()}")
+        st.write(f"{idx}. {item.strip()}", format="markdown")
+
 
