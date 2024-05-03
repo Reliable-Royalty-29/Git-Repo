b703ac7386bd0518805a722ddaeab2f1d43c7f9b
Dheeman Das <84588268+Reliable-Royalty-29@users.noreply.github.com>
2024-05-03 12:30:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import langchain_helper
 
 st.title("Restaurant Name Generator")
 
-cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican", "Arabic", "American"))
+cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican"))
 
 if cuisine:
     response = langchain_helper.generate_restaurant_name_and_items(cuisine)
@@ -12,3 +12,5 @@ if cuisine:
     st.write("**Menu Items**")
     for item in menu_items:
         st.write("-", item)
+
+
